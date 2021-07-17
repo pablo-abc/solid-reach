b3eb615779dcf3529052286bb5f8f5a7a455ea58
Pablo Berganza <40573613+pablo-abc@users.noreply.github.com>
2021-07-17 13:48:27 +0200

    feat(disclosure): add dev warning

--- packages/disclosure/src/Disclosure.tsx
+++ packages/disclosure/src/Disclosure.tsx
@@ -1,6 +1,7 @@
 import { createId } from '@solid-reach/utils';
-import { createSignal, JSX, mergeProps } from 'solid-js';
+import { createSignal, JSX, mergeProps, createEffect } from 'solid-js';
 import { DisclosureContextValue, DisclosureContext } from './context';
+import warning from 'tiny-warning';
 
 type DisclosureProps = {
   children: JSX.Element;
@@ -12,17 +13,29 @@ type DisclosureProps = {
 
 export default function Disclosure(props: DisclosureProps) {
   props = mergeProps({ defaultOpen: false }, props);
-  const wasControlled = props.open != null;
-  let isControlled = wasControlled;
+  const isControlled = () => props.open != null;
   const id = () => createId(props.id) || 'disclosure';
   const panelId = () => `panel---${id()}`;
   const [isOpen, setIsOpen] = createSignal(
-    isControlled ? (props.open as boolean) : (props.defaultOpen as boolean)
+    isControlled() ? (props.open as boolean) : (props.defaultOpen as boolean)
   );
+
+  if (__DEV__) {
+    const wasControlled = isControlled();
+    createEffect(() => {
+      warning(
+        !(
+          (isControlled() && !wasControlled) ||
+          (!isControlled() && wasControlled)
+        ),
+        'Disclosure is changing from controlled to uncontrolled. Disclosure should not switch from controlled to uncontrolled (or vice versa). Decide between using a controlled or uncontrolled Disclosure for the lifetime of the component. Check the `open` prop being passed in.'
+      );
+    });
+  }
 
   function onSelect() {
     props.onChange?.();
-    if (!isControlled) {
+    if (!isControlled()) {
       setIsOpen((isOpen) => !isOpen);
     }
   }
@@ -34,7 +47,7 @@ export default function Disclosure(props: DisclosureProps) {
     panelId,
   };
 
-  if (isControlled && props.open !== isOpen()) {
+  if (isControlled() && props.open !== isOpen()) {
     /*
      * If the component is controlled, we'll sync internal state with the
      * controlled state
